143c94994575fa07a4709e1fa00cf5c35ba43915
@gree180160 <1459287460@qq.com>
2016-10-31 08:52:57 +0800

    adjust
    adjust

---
+++ Survey/SurveyTableVCell.swift
@@ -0,0 +1,50 @@
+//
+//  SurveyTableVCell.swift
+//  Survey
+//
+//  Created by WillowRivers on 16/10/26.
+//  Copyright © 2016年 WillowRivers. All rights reserved.
+//
+
+import UIKit
+
+class SurveyTableVCell: UITableViewCell {
+    var questionLab : UILabel! ;
+    var options : UISegmentedControl! ;
+    var result : String! ;
+    override init(style : UITableViewCellStyle , reuseIdentifier : String?)
+    {
+        
+        super.init(style : UITableViewCellStyle.default , reuseIdentifier : reuseIdentifier) ;
+        self.creat() ;
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    func creat() -> Void
+    {
+        let cellFrame = self.frame ;
+        questionLab = UILabel(frame: CGRect(x: cellFrame.minX , y: cellFrame.minY + 2 , width: cellFrame.width , height: 30)) ;
+        questionLab.adjustsFontSizeToFitWidth = true ;
+        //questionLab.text = "您认为产品的供应周期是否能满足您的预期要求？"
+        
+        options = UISegmentedControl(items: ["1分","2分","3分","4分","5分"]) ;
+        options.frame = CGRect(x: cellFrame.minX , y: self.questionLab.frame.maxY , width: cellFrame.width , height: 30)
+        self.contentView.addSubview(questionLab) ;
+        self.contentView.addSubview(options) ;
+    }
+   /*
+    func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+*/
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        self.result = String(options.selectedSegmentIndex + 1) ;
+        print("in cell class result is \(result)")
+    }
+
+}
--- Survey/ViewController.swift
+++ Survey/ViewController.swift
@@ -8,18 +8,135 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDataSource , UITableViewDelegate , UITextViewDelegate {
+    var surveyTableV : UITableView! ;
+    
+    var suggestionTextV : UITextView! ;
+    var submit : UIButton! ;
+    override func viewDidLoad()
+    {
+        super.viewDidLoad() ;
+        self.view.backgroundColor = UIColor.white ;
+        
+        surveyTableV = UITableView(frame: CGRect(x : 5 , y : 0 , width : self.view.frame.width - 10 , height : self.view.frame.height * 27/30 ) , style: .plain) ;
+        surveyTableV.register(SurveyTableVCell.self , forCellReuseIdentifier: "CellID") ;
+        surveyTableV.rowHeight = 65 ;
+        surveyTableV.dataSource = self ;
+        surveyTableV.delegate = self ;
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let intrudctionLab = UILabel(frame: CGRect(x: 5 , y: surveyTableV.frame.maxY , width: self.view.frame.width * 4/5 , height: view.frame.height / 15)) ;
+        intrudctionLab.text = "other suggestions write follow" ;
+        
+        suggestionTextV = UITextView(frame: CGRect(x: 3 , y: intrudctionLab.frame.maxY , width: self.view.frame.width - 6 , height: self.view.frame.height * 1/5)) ;
+        suggestionTextV.layer.borderWidth = 2 ;
+        suggestionTextV.layer.cornerRadius = 10.0 ;
+        suggestionTextV.text = "Idiot dong retired right now" ;
+        suggestionTextV.delegate = self ;
+        
+        submit = UIButton(frame: CGRect(x: self.view.frame.width / 4 , y: suggestionTextV.frame.maxY + 5 , width: self.view.frame.width / 2 , height: self.view.frame.height / 15)) ;
+        submit.setTitle("submit", for: .normal) ;
+        submit.layer.cornerRadius = 20.0 ;
+        submit.backgroundColor = UIColor.red ;
+        submit.addTarget(self , action: #selector(ViewController.submitF), for: .touchUpInside) ;
+        
+        NotificationCenter.default.addObserver(self , selector: #selector(ViewController.keyboardAppear), name: NSNotification.Name.UIKeyboardWillShow , object: nil) ;
+        NotificationCenter.default.addObserver(self , selector: #selector(ViewController.keyboardDisappear), name: NSNotification.Name.UIKeyboardWillHide , object: nil) ;
+        
+        let scrollV = UIScrollView(frame: CGRect(x: 0 , y: 0, width: self.view.frame.width , height: view.frame.height )) ;
+        scrollV.contentSize = CGSize(width: view.frame.width , height: view.frame.height + intrudctionLab.frame.height + suggestionTextV.frame.height + submit.frame.height)
+        scrollV.isScrollEnabled = true ;
+        
+        scrollV.addSubview(suggestionTextV) ;
+        scrollV.addSubview(intrudctionLab) ;
+        scrollV.addSubview(surveyTableV) ;
+        scrollV.addSubview(submit) ;
+        self.view.addSubview(scrollV) ;
     }
-
-    override func didReceiveMemoryWarning() {
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        suggestionTextV.resignFirstResponder() ;
+    }
+    
+    func keyboardAppear(notification : NSNotification) -> Void
+    {
+        let useInfo : NSDictionary = notification.userInfo! as NSDictionary ;
+        let nsValue : NSValue = useInfo.object(forKey: UIKeyboardFrameEndUserInfoKey) as! NSValue ;
+        let frame = nsValue.cgRectValue.size ;
+        let kbHeight = frame.height ;
+        let offset = self.suggestionTextV.frame.maxY - (self.view.frame.height - kbHeight) ;
+        if offset > 0
+        {
+            self.view.frame = CGRect(x: 0 , y: -offset , width: self.view.frame.width , height: self.view.frame.height) ;
+        }
+        print("offset is \(offset)") ;
+    }
+    func keyboardDisappear(notification : NSNotification) -> Void
+    {
+         self.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.width , height: self.view.frame.height) ;
+    }
+    
+    
+    override var prefersStatusBarHidden: Bool{ return true} //隐藏状态栏
+    func submitF() -> Void {
+        print("the result is :") ;
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 30 ;
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellID", for: indexPath) as! SurveyTableVCell ;
+        cell.questionLab.text = String(indexPath.row) + ". " + "您认为产品的供应周期是否能满足您的预期要求？" ;
+        return cell ;
+    }
+    func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1 ;
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        print("outside result is :\((tableView.cellForRow(at: indexPath) as! SurveyTableVCell).result)") ;
+    }
 
+    /*
+     func textViewDidBeginEditing(_ textView: UITextView) -> Void {
+     let frame = textView.frame ;
+     let offset = frame.origin.y + 100 - (self.view.frame.height - 330) ;
+     if offset > 0
+     {
+     self.view.frame = CGRect(x: 0 , y: -offset , width: self.view.frame.width , height: self.view.frame.height) ;
+     }
+     print("mover keyboard") ;
+     }
+     func textViewDidEndEditing(_ textView: UITextView) {
+     self.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.width , height: self.view.frame.height) ;
+     }
+     */
+    //click space keyboard will disappear
+    
+    /*  // click return keyboard will disappear
+     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool
+     {
+     if text == "\n"
+     {
+     textView.resignFirstResponder() ;
+     return true
+     }
+     else
+     {
+     return false ;
+     }
+     
+     }
+     */
 
 }
 
